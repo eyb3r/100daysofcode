ec6c52f25767482399532c523f2d51ddb6135a98
eyb3r <eyber@redpoll.it>
2023-04-04 23:42:56 +0200

    end of day 20

---
+++ day-20-21-snake/snake.py
@@ -0,0 +1,39 @@
+from turtle import Turtle
+
+SEGMENT_SIZE = 20
+UP = 90
+LEFT = 180
+DOWN = 270
+RIGHT = 0
+
+class Snake:
+    def __init__(self, color='white'):
+        self.body = [Turtle(), Turtle(), Turtle()]
+        self.head = self.body[0]
+        for i, segment in enumerate(self.body):
+            segment.shape('square')
+            segment.setx(i * -SEGMENT_SIZE)
+            segment.color(color)
+            segment.penup()
+
+    def move(self):
+        for i in range(len(self.body)-1, 0, -1):
+            self.body[i].setx(self.body[i-1].xcor())
+            self.body[i].sety(self.body[i-1].ycor())
+        self.head.forward(SEGMENT_SIZE)
+
+    def up(self):
+        if not self.head.heading() % 180:
+            self.head.setheading(UP)
+
+    def down(self):
+        if not self.head.heading() % 180:
+            self.head.setheading(DOWN)
+
+    def right(self):
+        if self.head.heading() % 180:
+            self.head.setheading(RIGHT)
+
+    def left(self):
+        if self.head.heading() % 180:
+            self.head.setheading(LEFT)
