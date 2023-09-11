e80de29b166657055c7d7653b74871d24f17b909
eyb3r <eyber@redpoll.it>
2023-09-11 23:12:19 +0200

    day-30- password manager and NATO spelling machine updated with some exception handling stuff

--- day-26/main.py
+++ day-26/main.py
@@ -4,7 +4,11 @@ codes = pandas.read_csv('nato_phonetic_alphabet.csv')
 #Loop through rows of a data frame
 
 phonetics = {row.letter: row.code for (_, row) in codes.iterrows()}
-
-result = [phonetics[letter.upper()] for letter in input('Enter a word: ')]
-
-print(result)
+while True:
+    try:
+        result = [phonetics[letter.upper()] for letter in input('Enter a word: ')]
+    except KeyError:
+        print("Only letters please")
+    else:
+        print(result)
+        break
--- day-29-password-manager/main.py
+++ day-29-password-manager/main.py
@@ -2,6 +2,8 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
@@ -43,20 +45,53 @@ def validate_password(password):
 def store_password():
     # save password to a file
     entry = [website_field.get(), email_field.get(), password_field.get()]
+    website, email, password = entry  # allow easier handling
+    json_data = None
     entry_validation = [len(x) > 0 for x in entry]
     if False in entry_validation:
         messagebox.showerror(title='Error', message='Fill all the fields.')
     else:
-        msg = f'Is the entered data correct?\nUsername:{entry[1]}\n'\
-              f'Password:{entry[2]}'
-        confirmed = messagebox.askokcancel(title=entry[0], message=msg)
+        new_record = {
+            website: {
+                "email": email,
+                "password": password
+            }
+        }
+        try:
+            with open('database.json', 'r') as db:
+                json_data = json.load(db)
+                json_data.update(new_record)
+        except (FileNotFoundError, json.JSONDecodeError):
+            json_data = new_record
 
-        if confirmed:
-            with open('database.csv', 'a') as db:
-                db.write(','.join(entry)+'\n')
+        finally:
+            with open('database.json', 'w') as db:
+                json.dump(json_data, db, indent=4)
             # clear the form
             website_field.delete(0, END)
             password_field.delete(0, END)
+
+
+def find_password():
+    website = website_field.get()
+    try:
+        if website == "":
+            raise ValueError("Empty value is not allowed")
+
+        with open('database.json', 'r') as db:
+            json_data = json.load(db)
+
+        messagebox.showinfo(title=f'{website} entry', message=f'email:{json_data[website]["email"]}\n'
+                                                              f'password:{json_data[website]["password"]}')
+
+    except FileNotFoundError:
+        messagebox.showinfo(title=f'missing database', message=f'No database found.')
+    except KeyError:
+        messagebox.showinfo(title=f'missing key for {website}', message=f'No entry for {website} has been found.')
+    except ValueError:
+        messagebox.showinfo(title=f'missing website', message=f'Enter a website to search for!')
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 
@@ -66,22 +101,24 @@ window.config(padx=50, pady=50)
 
 logo = PhotoImage(file='logo.png')
 canvas = Canvas(highlightthickness=0, width=200, height=200)
-logo_widget = canvas.create_image(100, 100, image=logo )
+logo_widget = canvas.create_image(100, 100, image=logo)
 canvas.grid(column=1, row=0)
 
 website_label = Label(text='Website:')
 website_label.grid(column=0, row=1, sticky='e')
 
-website_field = Entry(width=35)
-website_field.grid(column=1, row=1, columnspan=2)
+website_field = Entry(width=25)
+website_field.grid(column=1, row=1)
+
+search_button = Button(text='Search', command=find_password, width=6)
+search_button.grid(column=2, row=1)
 
 email_label = Label(text='Email/Username:')
 email_label.grid(column=0, row=2, sticky='e')
 
 email_field = Entry(width=35)
-email_field.insert(0,'testmail@gmail.com')
+email_field.insert(0, 'testmail@gmail.com')
 email_field.grid(column=1, row=2, columnspan=2)
-
 
 password_label = Label(text='Password:')
 password_label.grid(column=0, row=3, sticky='e')
@@ -94,10 +131,5 @@ generate_button.grid(column=2, row=3)
 
 add_button = Button(text='Add', width=33, command=store_password)
 add_button.grid(column=1, row=4, columnspan=2)
-
-
-
-
-
 
 window.mainloop()
